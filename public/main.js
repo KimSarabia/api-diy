6ef084d7a9799ae7baf08388ced1491ec856a08c
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-19 22:49:56 -0700

    cube is working :+1:

--- public/main.js
+++ public/main.js
@@ -3,12 +3,29 @@ $(function(){
   console.log('works:', 'hello main!');
   document.getElementsByTagName('h1')[0].textContent = "WORKING";
   $('button#getSquare').click(getSquare);
+  $('button#getCube').click(getCube);
+  $('button#getProduct').click(getProduct);
+  $('button#getQuotient').click(getQuotient);
+  $('button#getDiff').click(getDiff);
+  $('button#getSum').click(getSum);
 });
 
 function getSquare(){
   var num = $('#num').val();
 
   $.get(`/math/square/${num}`)
+  .done(function(data){
+    console.log('data:',data);
+  })
+  .fail(function(error) {
+    console.log('error:', error);
+  });
+}
+
+function getCube(){
+  var num = $('#num').val();
+
+  $.get(`/math/cube/${num}`)
   .done(function(data){
     console.log('data:',data);
   })
