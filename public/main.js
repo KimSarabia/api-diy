cf430780d6087781d2ba0246de91b9e33b440b54
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-20 00:33:28 -0700

    jquery works!

--- public/main.js
+++ public/main.js
@@ -1,86 +1,99 @@
 'use strict';
-$(function(){
-  console.log('works:', 'hello main!');
-  document.getElementsByTagName('h1')[0].textContent = "WORKING";
-  $('button#getSquare').click(getSquare);
-  $('button#getCube').click(getCube);
-  $('button#getProduct').click(getProduct);
-  $('button#getQuotient').click(getQuotient);
-  $('button#getDiff').click(getDiff);
-  $('button#getSum').click(getSum);
+$(function() {
+    console.log('works:', 'hello main!');
+    document.getElementsByTagName('h1')[0].textContent = "WORKING";
+    $('button#getSquare').click(getSquare);
+    $('button#getCube').click(getCube);
+    $('button#getProduct').click(getProduct);
+    $('button#getQuotient').click(getQuotient);
+    $('button#getDiff').click(getDiff);
+    $('button#getSum').click(getSum);
+    $('button#getGravatar').click(getGravatar);
 });
 
-function getSquare(){
-  var num = $('#num').val();
+function getSquare() {
+    var num = $('#num').val();
 
-  $.get(`/math/square/${num}`)
-  .done(function(data){
-    console.log('data:',data);
-  })
-  .fail(function(error) {
-    console.log('error:', error);
-  });
+    $.get(`/math/square/${num}`)
+        .done(function(data) {
+            console.log('data:', data);
+            $('#squareResult').text(data);
+        })
+        .fail(function(error) {
+            console.log('error:', error);
+        });
 }
 
-function getCube(){
-  var num = $('#num').val();
+function getCube() {
+    var num = $('#num').val();
 
-  $.get(`/math/cube/${num}`)
-  .done(function(data){
-    console.log('data:',data);
-  })
-  .fail(function(error) {
-    console.log('error:', error);
-  });
+    $.get(`/math/cube/${num}`)
+        .done(function(data) {
+            console.log('data:', data);
+            $('#cubeResult').text(data);
+
+        })
+        .fail(function(error) {
+            console.log('error:', error);
+        });
 }
 
-function getProduct(){
-  var multiplicand = $('#multiplicand').val();
-  var multiplier = $('#multiplier').val();
+function getProduct() {
+    var multiplicand = $('#multiplicand').val();
+    var multiplier = $('#multiplier').val();
 
-  $.get(`/math/product/${multiplicand}/${multiplier}`)
-  .done(function(data){
-    console.log('data:',data);
-  })
-  .fail(function(error) {
-    console.log('error:', error);
-  });
+    $.get(`/math/product/${multiplicand}/${multiplier}`)
+        .done(function(data) {
+            console.log('data:', data);
+            $('#productResult').text(data);
+        })
+        .fail(function(error) {
+            console.log('error:', error);
+        });
 }
 
-function getQuotient(){
-  var divisor = $('#divisor').val();
-  var dividend = $('#dividend').val();
+function getQuotient() {
+    var divisor = $('#divisor').val();
+    var dividend = $('#dividend').val();
 
-  $.get(`/math/quotient/${divisor}/${dividend}`)
-  .done(function(data){
-    console.log('data:',data);
-  })
-  .fail(function(error) {
-    console.log('error:', error);
-  });
+    $.get(`/math/quotient/${divisor}/${dividend}`)
+        .done(function(data) {
+            console.log('data:', data);
+            $('#quotientResult').text(data);
+        })
+        .fail(function(error) {
+            console.log('error:', error);
+        });
 }
 
-function getSum(){
-  var addendOne = $('#addendOne').val();
-  var addendTwo = $('#addendTwo').val();
+function getSum() {
+    var addendOne = $('#addendOne').val();
+    var addendTwo = $('#addendTwo').val();
 
-  $.get(`/math/sum/${addendOne}/${addendTwo}`)
-  .done(function(data){
-    console.log('data:',data);
-  })
-  .fail(function(error) {
-    console.log('error:', error);
-  });
+    $.get(`/math/sum/${addendOne}/${addendTwo}`)
+        .done(function(data) {
+            console.log('data:', data);
+            $('#sumResult').text(data);
+        })
+        .fail(function(error) {
+            console.log('error:', error);
+        });
 }
-function getDiff(){
-  var minuend = $('#minuend').val();
-  var subtrahend = $('#subtrahend').val();
 
-  $.get(`/math/diff/${minuend}/${subtrahend}`)
-  .done(function(data){
-    console.log('data:',data);
-  })
-  .fail(function(error) {
-    console.log('error:', error);
-  });
+function getDiff() {
+    var minuend = $('#minuend').val();
+    var subtrahend = $('#subtrahend').val();
+
+    $.get(`/math/diff/${minuend}/${subtrahend}`)
+        .done(function(data) {
+            console.log('data:', data);
+            $('#diffResult').text(data);
+        })
+        .fail(function(error) {
+            console.log('error:', error);
+        });
+}
+
+function getGravatar() {
+    console.log('gravatar', 'gravatar!');
 }
--- server.js
+++ server.js
@@ -5,34 +5,38 @@ const PORT = 8080;
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer((req,res) => {
-  var params = req.url.split('/');
-  params.shift();
-  var resource = params.shift().toLowerCase();
-  console.log(resource);
+var server = http.createServer((req, res) => {
+    var params = req.url.split('/');
+    params.shift();
+    var resource = params.shift().toLowerCase();
+    console.log(resource);
 
-  switch(resource) {
-    case 'math': require('./math')(params,res);break;
-    case 'str': require('./str')(req, res, params); break;
-    case '':
-     var data = fs.readFileSync('./public/index.html');
-     res.end( data.toString() );
-     break;
+    switch (resource) {
+        case 'math':
+            require('./math')(params, res);
+            break;
+        case 'str':
+            require('./str')(req, res, params);
+            break;
+        case '':
+            var data = fs.readFileSync('./public/index.html');
+            res.end(data.toString());
+            break;
 
-    default:
-    fs.readFile(`./public/${resource}`, (err,data) => {
-      if(err) {
-        res.statusCode = 404;
-        res.write('Oops. Something went wrong.');
-        res.end('\n');
-      } else {
-        res.end( data.toString() );
-      }
-    });
-  }
+        default:
+            fs.readFile(`./public/${resource}`, (err, data) => {
+                if (err) {
+                    res.statusCode = 404;
+                    res.write('Oops. Something went wrong.');
+                    res.end('\n');
+                } else {
+                    res.end(data.toString());
+                }
+            });
+    }
 });
 
-server.listen(PORT, function(err){
-  if(err) return console.log('error!:', err);
-  console.log(`Server listening on port ${PORT}`);
+server.listen(PORT, function(err) {
+    if (err) return console.log('error!:', err);
+    console.log(`Server listening on port ${PORT}`);
 });
